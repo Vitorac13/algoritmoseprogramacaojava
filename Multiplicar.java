9d5388ac2dd51dc51760c8783da9d6682b86bcc8
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-04-16 00:06:29 +0000

    programa_multiplicar

---
+++ Multiplicar.java
@@ -0,0 +1,10 @@
+void main(){
+
+    int n1 = Integer.parseInt(IO.readln("Digite o primeiro número: "));
+    int n2 = Integer.parseInt(IO.readln("Digite o segundo número: "));
+
+    int result = n1 * n2;
+
+    IO.println(n1 + " x " + n2 + " = " + result);
+
+}
