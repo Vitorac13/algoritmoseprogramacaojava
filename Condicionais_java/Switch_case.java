2f8f054ff6a51a87b931094b0b687960d0cbc2c7
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-04-22 23:12:35 +0000

    switch case

---
+++ Condicionais_java/Switch_case.java
@@ -0,0 +1,12 @@
+void main(){
+
+    int opcao = Integer.parseInt(IO.readln("Selecione uma opção: "));
+
+    switch(opcao){
+        case<1>;
+        break;
+        default:
+            IO.println("Operação inválida.");
+    }
+
+}
---
+++ Condicionais_java/triangulos.java
@@ -0,0 +1,7 @@
+void main(){
+    
+    int n1 = Integer.parseInt(System.in.printf("DIgite um número: "));
+    
+    System.out.println(n1);
+
+}
