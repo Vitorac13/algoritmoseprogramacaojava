2e791b6f5e662e222638e932cb0defefbce361de
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-04-27 17:04:49 +0000

    while no switch case

--- Condicionais_java/Switch_case.java
+++ Condicionais_java/Switch_case.java
@@ -1,22 +1,28 @@
 void main(){
 
-    int num1 = Integer.parseInt(IO.readln("Digite o primeiro número: "));
-    int num2 = Integer.parseInt(IO.readln("Digite o segundo número: "));
+    while (true){
 
-    IO.println("\nMenu:");
-    IO.println("1 - Somar os números digitados.");
-    IO.println("2 - Subtrair os números digitados. \n");
-    int opcao = Integer.parseInt(IO.readln("Selecione uma opção: "));
+        IO.println("\nMenu:");
+        IO.println("1 - Somar os números digitados.");
+        IO.println("2 - Subtrair os números digitados.");
+        IO.println("0 - Sair. \n");
+        int opcao = Integer.parseInt(IO.readln("Selecione uma opção: "));
+        
+        if (opcao == 0) {IO.println("\nPrograma encerrado"); System.exit(0);}
 
-    switch(opcao){
-        case 1:
-            IO.println("\mResultado da soma: " + (num1 + num2));
-            break;
-        case 2:
-            IO.println("\nResultado da subtração: " + (num1 - num2));
-            break;
-        default:
-            IO.println("\nOperação inválida.");
+        int num1 = Integer.parseInt(IO.readln("\nDigite o primeiro número: "));
+        int num2 = Integer.parseInt(IO.readln("Digite o segundo número: "));
+
+        switch(opcao){
+            case 1:
+                IO.println("\nResultado da soma: " + (num1 + num2));
+                break;
+            case 2:
+                IO.println("\nResultado da subtração: " + (num1 - num2));
+                break;
+            default:
+                IO.println("\nOperação inválida.");
+        }
     }
 
 }
