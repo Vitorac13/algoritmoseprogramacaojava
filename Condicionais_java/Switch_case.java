85d64702950c929965f8fb2f17b716fa0cca4dbb
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-04-27 16:45:16 +0000

    soma e subtração switch case

--- Condicionais_java/Switch_case.java
+++ Condicionais_java/Switch_case.java
@@ -1,12 +1,22 @@
 void main(){
 
+    int num1 = Integer.parseInt(IO.readln("Digite o primeiro número: "));
+    int num2 = Integer.parseInt(IO.readln("Digite o segundo número: "));
+
+    IO.println("\nMenu:");
+    IO.println("1 - Somar os números digitados.");
+    IO.println("2 - Subtrair os números digitados. \n");
     int opcao = Integer.parseInt(IO.readln("Selecione uma opção: "));
 
     switch(opcao){
-        case<1>;
-        break;
+        case 1:
+            IO.println("\mResultado da soma: " + (num1 + num2));
+            break;
+        case 2:
+            IO.println("\nResultado da subtração: " + (num1 - num2));
+            break;
         default:
-            IO.println("Operação inválida.");
+            IO.println("\nOperação inválida.");
     }
 
 }
