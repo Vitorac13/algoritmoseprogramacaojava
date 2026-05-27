3f23e36a8fa2a4ca26cc9b3241f62cfb5d034c01
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-05-27 22:59:56 +0000

    programas de pesquisa

---
+++ Pesquisa/Equals.java
@@ -0,0 +1,9 @@
+void main() {
+    
+    String a = IO.readln("Informe um nome... : ");
+    String b = IO.readln("Informe outro nome : ");
+    
+    IO.println("          a.equals(b) = " + a.equals(b));
+    IO.println("a.equalsIgnoreCase(b) = " + a.equalsIgnoreCase(b));
+        
+  }
