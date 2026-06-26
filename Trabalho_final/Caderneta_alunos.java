55b451a1a3d41d758571566e44f470d04db45cdc
Vitor Costa <82245170+Vitorac13@users.noreply.github.com>
2026-06-26 23:07:15 +0000

    menu cadastro alunos

--- Trabalho_final/Caderneta_alunos.java
+++ Trabalho_final/Caderneta_alunos.java
@@ -1,4 +1,19 @@
+//Vitor Almeida Costa
+
 void cadastrarAluno(){
+    IO.println("=-=-=-=- Cadastro de aluno -=-=-=-=");
+    for(int i=0; i<20; i++){
+        nomesAlunos[i] = IO.readln("Digite o nome do " + (i+1) + "º aluno: ");
+        for(int j=0; j<3; j++){
+            aluno[i][j] = Float.parseFloat(IO.readln("Digite a " + (j+1) + "º nota de " + nomesAlunos[i] + ": "));
+        }
+        aluno[i][0] = (aluno[i][0][0] + aluno[i][0][1] + aluno[i][0][2]) / 3; //Calculo média
+        if(aluno[i][0] >= 7.0){
+            nomesAlunos[i][0] = "Aprovado";
+        } else {
+            nomesAlunos[i][0] = "Reprovado";
+        }
+    }
 
 }
 
@@ -11,29 +26,39 @@ void pesquisarAluno(){
 }
 
 void main(){
-
-int opcao = 1;
-while(opcao != 4){  
-
-    IO.println("1 - Cadastrar Alunos e Médias");
-    IO.println("2 - Exibir Lista de Alunos e  Notas");
-    IO.println("3 - Buscar Aluno");
-    IO.println("4 - Sair");
-    opcao = Integer.ParseInt(IO.readln("Escolha uma opção: "));
-    switch(opcao){
-        case 1:
-            cadastrarAluno();
-            break;
-        case 2:
-            listarAlunos();
-            break;
-        case 3:
-            pesquisarAluno();
-            break;
-        case 4:
-            IO.println("Programa Encerrado.");
-            break;
-        default:
-            IO.println("Opção inválida!");
+    
+    var aluno = new float[20][1][3];
+    //[nºaluno] - [media] - [notas]
+    
+    var nomesAlunos = new String[20][1];
+    //[nomeAluno] - [situacao]
+    
+    int opcao = 1;
+    while(opcao != 4){  
+        
+        IO.println("=== MENU PRINCIPAL: GESTÃO DE TURMA ===");
+        IO.println("1 - Cadastrar Alunos e Médias");
+        IO.println("2 - Exibir Lista de Alunos e  Notas");
+        IO.println("3 - Buscar Aluno");
+        IO.println("4 - Sair");
+        
+        opcao = Integer.parseInt(IO.readln("Escolha uma opção: "));
+        
+        switch(opcao){
+            case 1:
+                cadastrarAluno();
+                break;
+            case 2:
+                listarAlunos();
+                break;
+            case 3:
+                pesquisarAluno();
+                break;
+            case 4:
+                IO.println("Programa Encerrado.");
+                break;
+            default:
+                IO.println("Opção inválida!");
+        }
     }
 }
